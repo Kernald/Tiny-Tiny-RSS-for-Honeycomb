6a4bca7333b7936be25391217d03aedb3c968ba8
Marc Plano-Lesay <marc.planolesay@gmail.com>
2014-10-27 11:47:54 +0100

    Apply status bar tint to Lollipop

--- org.fox.ttrss/src/main/java/org/fox/ttrss/CommonActivity.java
+++ org.fox.ttrss/src/main/java/org/fox/ttrss/CommonActivity.java
@@ -165,7 +165,7 @@ public class CommonActivity extends ActionBarActivity {
 	}
 
 	public void setStatusBarTint() {
-		if (android.os.Build.VERSION.SDK_INT == android.os.Build.VERSION_CODES.KITKAT &&
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT &&
                 !m_prefs.getBoolean("full_screen_mode", false)) {
 
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
